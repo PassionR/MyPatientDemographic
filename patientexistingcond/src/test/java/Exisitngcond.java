0cca10e168d582a622b38f53bc313583e931fa01
PassionR <francisreena@yahoo.com>
2018-05-27 21:37:40 -0600

    have added 2 new methods

--- patientexistingcond/src/test/java/Exisitngcond.java
+++ patientexistingcond/src/test/java/Exisitngcond.java
@@ -7,6 +7,14 @@ public class Exisitngcond {
   public void f() {
 	  System.out.println("print exisitng ocndion");
   }
+	 @Test
+  public void f1() {
+	  System.out.println("print exisitng ocndion 1");
+  }
+	 @Test
+  public void f2() {
+	  System.out.println("print exisitng ocndion 2");
+  }
   @BeforeMethod
   public void beforeMethod() {
   }
