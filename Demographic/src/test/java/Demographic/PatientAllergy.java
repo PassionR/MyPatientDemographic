3c7f12b2c50b62b8fff38233675cb12c29715bc5
freena <freena@10.0.0.214>
2018-05-27 21:10:41 -0600

    enter commment

---
+++ Demographic/src/test/java/Demographic/PatientAllergy.java
@@ -0,0 +1,20 @@
+package Demographic;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class PatientAllergy {
+  @Test
+  public void f() {
+	  System.out.println("print allergy");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
---
+++ patientexistingcond/src/test/java/Exisitngcond.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class Exisitngcond {
+  @Test
+  public void f() {
+	  System.out.println("print exisitng ocndion");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
